feec4ee7d755b60f5ea9c0d6304bf32ef99fe048
unknown <aindovina2@yahoo.com>
2022-02-01 11:15:07 -0500

    mobile view

---
+++ app/app.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector('.menu-container');
+const sidebarOverlay = document.querySelector('.sidebar-bg-overlay');
+const sidebar = document.querySelector('.sidebar');
+
+menuBtn.addEventListener('click', () => {
+  if (!menuBtn.classList.contains('close')) {
+    menuBtn.classList.add('close');
+    menuBtn.ariaLabel = 'close menu';
+    sidebarOverlay.classList.add('active');
+    sidebar.classList.add('active');
+  } else {
+    menuBtn.classList.remove('close');
+    menuBtn.ariaLabel = 'open menu';
+    sidebarOverlay.classList.remove('active');
+    sidebar.classList.remove('active');
+  }
+})
