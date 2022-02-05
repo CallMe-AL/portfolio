16e959c6e292a735298b42faf90f6500bca5cb2e
unknown <aindovina2@yahoo.com>
2022-02-04 19:29:55 -0500

    scroll to top button added

--- app/app.js
+++ app/app.js
@@ -1,6 +1,23 @@
 const menuBtn = document.querySelector('.menu-container');
 const sidebarOverlay = document.querySelector('.sidebar-bg-overlay');
 const sidebar = document.querySelector('.sidebar-container');
+const scrollBtn = document.querySelector('.scroll-btn');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 500) {
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+});
 
 menuBtn.addEventListener('click', () => {
   if (!menuBtn.classList.contains('close')) {
